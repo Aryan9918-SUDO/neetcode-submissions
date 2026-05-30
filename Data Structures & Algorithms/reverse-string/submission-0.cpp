d67f6108a79b5a94b13db1840acf92e2f621c2c0
Aryan kumar  singh <f20230362@goa.bits-pilani.ac.in>
2026-05-30 21:27:22 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int low = 0;
+        int high= n-1;
+        while(low<=high){
+            int temp= s[low];
+            s[low]=s[high];
+            s[high]=temp;
+            low++;
+            high--;
+        }
+        return ;
+    }
+};
