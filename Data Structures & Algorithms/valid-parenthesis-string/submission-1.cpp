32fbfab2bcf6a2935bef9ab09cb8d6a04e9685c3
Aryan kumar  singh <f20230362@goa.bits-pilani.ac.in>
2026-06-25 23:00:25 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int>open;
+        stack<int>star;
+        for(int i =0;i<s.size();i++){
+            char ch = s[i];
+            if(ch=='('){
+                open.push(i);
+            }
+            else if( ch=='*'){
+                star.push(i);
+            }
+            else{
+                if(!open.empty()){
+                    open.pop();
+                }
+                else if(!star.empty()){
+                    star.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        while(!open.empty() && !star.empty()){
+            if(open.top()>star.top()){
+                return false;
+            }
+            open.pop();
+            star.pop();
+        }
+        return open.empty();
+        
+    }
+};
